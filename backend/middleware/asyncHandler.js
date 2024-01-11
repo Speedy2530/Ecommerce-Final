d26951a427100cd47111298346177d97d30e5426
Saihej Singh <ssaihej@gmail.com>
2024-01-11 02:34:00 -0500

    brackets

--- backend/middleware/asyncHandler.js
+++ backend/middleware/asyncHandler.js
@@ -3,7 +3,7 @@
 const asyncHandler = fn => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next);
 }
-//function takes in request resolve next, and if it resolves, it calls the next piece of middleware
+//function takes in (request resolve next), and if it resolves, it calls the next piece of middleware
 //simplifies code by removing the need for a bunch of try/catch blocks
 
 export default asyncHandler
